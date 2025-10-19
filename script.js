198f6e1ebc874fcb20df791b163c9d1ea6388c02
Shareefashubber40 <202404008@student.polytechnic.bh>
2025-10-19 17:44:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.getElementById("book").onsubmit= function () {alert("we will contact you soon"); return false;};
-document.getElementsByTagName("th")[0].onclick= function () {alert("prices include VAT");};
+document.getElementById("book"). onsubmit= function () {alert("we will contact you soon"); return false;};
+document.getElementsByTagName("th")[0]. onclick= function () {alert("prices include VAT");};
